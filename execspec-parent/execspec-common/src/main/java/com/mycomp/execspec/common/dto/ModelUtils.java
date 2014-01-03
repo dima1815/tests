dedff7fa20fa90405e2ac951448c04086c6c368a
dima1815 <d.stasyuk@gmail.com>
2014-01-03 15:58:06 +0100

    Update ModelUtils.java

--- execspec-parent/execspec-common/src/main/java/com/mycomp/execspec/common/dto/ModelUtils.java
+++ execspec-parent/execspec-common/src/main/java/com/mycomp/execspec/common/dto/ModelUtils.java
@@ -1,7 +1,33 @@
 package com.mycomp.execspec.common.dto;
 
+import com.mycomp.execspec.common.domain.Story;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by dima on 04/01/14.
+ * Contains utility methods useful while working with model objects.
+ *
+ * @author stasyukd
+ * @since 2.0.0-SNAPSHOT
  */
 public class ModelUtils {
+
+    public static StoriesPayload toStoriesPayload(List<? extends Story> stories) {
+
+        List<StoryModel> storyModels = toModels(stories);
+        StoriesPayload storiesPayload = new StoriesPayload(storyModels);
+        return storiesPayload;
+    }
+
+    public static List<StoryModel> toModels(List<? extends Story> stories) {
+
+        List<StoryModel> storyModels = new ArrayList<StoryModel>(stories.size());
+        for (Story story : stories) {
+            StoryModel storyModel = new StoryModel(story);
+            storyModels.add(storyModel);
+        }
+
+        return storyModels;
+    }
 }

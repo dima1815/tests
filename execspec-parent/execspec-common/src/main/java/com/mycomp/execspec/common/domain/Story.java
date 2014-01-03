a1171209e780be914ce5a404f0bd13c6c63ed0ae
dima1815 <d.stasyuk@gmail.com>
2014-01-03 15:57:32 +0100

    Update Story.java

--- execspec-parent/execspec-common/src/main/java/com/mycomp/execspec/common/domain/Story.java
+++ execspec-parent/execspec-common/src/main/java/com/mycomp/execspec/common/domain/Story.java
@@ -1,7 +1,22 @@
 package com.mycomp.execspec.common.domain;
 
-/**
- * Created by dima on 04/01/14.
- */
-public class Story {
+public interface Story {
+
+    int getID();
+
+    String getIssueKey();
+
+    void setIssueKey(String issueKey);
+
+    String getMeta();
+
+    void setMeta(String storyText);
+
+    String getNarrative();
+
+    void setNarrative(String narrative);
+
+    String getLastRunStatus();
+
+    void setLastRunStatus(String lastRunStatus);
 }

8fc730027be7ea302c4f97ee29feef6da84ecbd2
dima1815 <d.stasyuk@gmail.com>
2014-01-03 16:02:28 +0100

    Update StoryService.java

--- execspec-parent/execspec-jira-plugin/src/main/java/com/mycomp/execspec/jiraplugin/service/StoryService.java
+++ execspec-parent/execspec-jira-plugin/src/main/java/com/mycomp/execspec/jiraplugin/service/StoryService.java
@@ -1,7 +1,26 @@
 package com.mycomp.execspec.jiraplugin.service;
 
-/**
- * Created by dima on 04/01/14.
- */
-public class StoryService {
+import com.atlassian.activeobjects.tx.Transactional;
+import com.mycomp.execspec.common.dto.StoryModel;
+
+import java.util.List;
+
+@Transactional
+public interface StoryService {
+
+    StoryModel create(StoryModel storyModel, long issueId);
+
+    StoryModel create(StoryModel storyModel, String issueKey);
+
+    StoryModel update(StoryModel storyModel);
+
+    List<StoryModel> all();
+
+    List<StoryModel> findByIssueKey(String issueKey);
+
+    StoryModel findById(Long storyId);
+
+    void delete(Long storyId);
 }
+
+

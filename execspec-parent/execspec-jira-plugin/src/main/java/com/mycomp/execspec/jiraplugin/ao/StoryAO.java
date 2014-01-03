288d4e597f96f56621ee96b1be140ddd6e34210d
dima1815 <d.stasyuk@gmail.com>
2014-01-03 15:59:25 +0100

    Update StoryAO.java

--- execspec-parent/execspec-jira-plugin/src/main/java/com/mycomp/execspec/jiraplugin/ao/StoryAO.java
+++ execspec-parent/execspec-jira-plugin/src/main/java/com/mycomp/execspec/jiraplugin/ao/StoryAO.java
@@ -1,7 +1,23 @@
 package com.mycomp.execspec.jiraplugin.ao;
 
+import com.mycomp.execspec.common.domain.Story;
+import net.java.ao.Preload;
+import net.java.ao.RawEntity;
+import net.java.ao.schema.AutoIncrement;
+import net.java.ao.schema.NotNull;
+import net.java.ao.schema.PrimaryKey;
+
 /**
- * Created by dima on 04/01/14.
+ * Entity interface has to be first in the list of extended interfaces as the getID() method has to be taken from it
+ * as it contains relevant AO annotations.
  */
-public class StoryAO {
+@Preload
+public interface StoryAO extends RawEntity<Integer>, Story {
+
+    @Override
+    @AutoIncrement
+    @NotNull
+    @PrimaryKey("ID")
+    public int getID();
+
 }

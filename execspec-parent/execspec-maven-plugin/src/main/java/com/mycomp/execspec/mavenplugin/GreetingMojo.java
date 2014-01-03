2838661cfdc6caf431ee30a988b2a6f81efff65c
dima1815 <d.stasyuk@gmail.com>
2014-01-03 16:13:30 +0100

    Update GreetingMojo.java

--- execspec-parent/execspec-maven-plugin/src/main/java/com/mycomp/execspec/mavenplugin/GreetingMojo.java
+++ execspec-parent/execspec-maven-plugin/src/main/java/com/mycomp/execspec/mavenplugin/GreetingMojo.java
@@ -1,7 +1,31 @@
 package com.mycomp.execspec.mavenplugin;
 
 /**
- * Created by dima on 04/01/14.
+ * TODO - add at least one line of java doc comment.
+ *
+ * @author stasyukd
+ * @since 2.0.0-SNAPSHOT
  */
-public class GreetingMojo {
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Says "Hi" to the user.
+ */
+@Mojo(name = "sayhi")
+public class GreetingMojo extends AbstractMojo {
+
+    /**
+     * The greeting to display.
+     */
+    @Parameter( property = "sayhi.greeting", defaultValue = "Hello World haha!" )
+    private String greeting;
+
+    public void execute() throws MojoExecutionException {
+        getLog().info(greeting);
+    }
+
 }

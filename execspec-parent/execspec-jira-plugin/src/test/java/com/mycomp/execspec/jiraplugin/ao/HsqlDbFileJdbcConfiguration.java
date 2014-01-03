90417243752aaec5937a793ffdd2ed498db5d0c3
dima1815 <d.stasyuk@gmail.com>
2014-01-03 16:11:34 +0100

    Update HsqlDbFileJdbcConfiguration.java

--- execspec-parent/execspec-jira-plugin/src/test/java/com/mycomp/execspec/jiraplugin/ao/HsqlDbFileJdbcConfiguration.java
+++ execspec-parent/execspec-jira-plugin/src/test/java/com/mycomp/execspec/jiraplugin/ao/HsqlDbFileJdbcConfiguration.java
@@ -1,7 +1,34 @@
 package com.mycomp.execspec.jiraplugin.ao;
 
+import net.java.ao.test.jdbc.JdbcConfiguration;
+
 /**
- * Created by dima on 04/01/14.
+ * Configuration for HSQL DB that uses file, to be used in tests.
+ *
+ * @author stasyukd
+ * @since 2.0.0-SNAPSHOT
  */
-public class HsqlDbFileJdbcConfiguration {
+public class HsqlDbFileJdbcConfiguration implements JdbcConfiguration {
+
+    @Override
+    public String getUrl() {
+        return "jdbc:hsqldb:hsql://localhost/fil_db";
+    }
+
+    @Override
+    public String getSchema() {
+
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return "sa";
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
 }

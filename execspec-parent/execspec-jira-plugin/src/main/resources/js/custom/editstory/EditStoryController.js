052fb05ad80e101722f1accfb9c3e95fcf999f40
dima1815 <d.stasyuk@gmail.com>
2014-01-19 01:15:03 +0100

    test - javascript organisation

--- execspec-parent/execspec-jira-plugin/src/main/resources/js/custom/editstory/EditStoryController.js
+++ execspec-parent/execspec-jira-plugin/src/main/resources/js/custom/editstory/EditStoryController.js
@@ -1,26 +1,20 @@
 function EditStoryController(){
 
-    console.log("-> EditStoryController.init");
-
     var $this = this;
 
-    this.hello = function () {
-        alert("hello from controller");
-    }
-
     this.init = function () {
+        console.log("-> EditStoryController.init");
         var model = new EditStoryModel();
         model.init();
         var view = new EditStoryView($this);
         view.init();
-        view.hello();
         console.log("<- EditStoryController.init");
     }
 }
 
 AJS.$(function() {
-    var editStoryController = new EditStoryController()
-    editStoryController.init();
+    var ctr = new EditStoryController()
+    ctr.init();
 });
 
 
